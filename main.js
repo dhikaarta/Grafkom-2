d37f6fd700ddafe83f31bd1ff608a48e5f578ccc
Andhika Arta Aryanto <71170262+dhikaarta@users.noreply.github.com>
2023-03-23 14:32:54 +0700

    feat : oblique

--- main.js
+++ main.js
@@ -319,7 +319,17 @@ function main() {
             var zFar = 2000.0;
             
             projectionMatrix = m4.perspective(canvasState.fieldOfViewRadians, aspect, zNear, zFar);
+        } else {
+            //oblique
+            //oblique pada intinya melakukan shear pada matrix ortho
+            
+            var n = m4.shear(64 ,64)
+            var m = m4.orthographic(-200, 200, -200, 200, -400, 400)
+            projectionMatrix = m4.multiply(m,n)
+            projectionMatrix = m4.translate(projectionMatrix, 50, -155, 0.5)
+            projectionMatrix = m4.xRotate(projectionMatrix, degToRad(180))
         }
+        console.log(projectionMatrix)
         
         var cameraMatrix = m4.yRotation(canvasState.cameraAngleRadians);
         cameraMatrix = m4.translate(cameraMatrix, 0, 0, canvasState.radius * 1.5);
